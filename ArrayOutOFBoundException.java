e07ed1f886550fcce84efc88561d043f7dc7061e
Abhinav Awasthi <56489028+abhinavawasthi1729@users.noreply.github.com>
2020-10-05 11:50:09 +0530

    Update ArrayOutOFBoundException.java

--- ArrayOutOFBoundException.java
+++ ArrayOutOFBoundException.java
@@ -13,11 +13,26 @@ class  ArrayOutOFBoundException
 			a[i]=scan.nextInt();
 		}
 		System.out.println();
-		for (i=0;i<=5 ;i++)
-		{
-			System.out.println(a[i]);
+		
+		//solution for this is exception handling used to prevent terminating the program
+		//we can use try-catch to catch exception
+		try{
+			for (i=0;i<=5 ;i++)
+			{
+				System.out.println(a[i]);
 			
+			}
 		}
-
+		catch(ArrayOutOFBoundException e){
+			System.out.println("You are excessing the invalid index. There is nothing there :) \n "+e.message());
+		}
+		//After that program start excuting from here instead of terminating
+		/*
+		
+		Write here any lines of code
+		
+		
+		*/
+			
 	}
 }
